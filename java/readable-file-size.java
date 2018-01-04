95d91a71573573fe1d3694eb4319f73d53102b64
unknown <rschmidt@W0420494.ads.lwl.org>
2018-01-04 11:32:30 +0100

    neu

---
+++ java/readable-file-size.java
@@ -0,0 +1,8 @@
+// pretty print file size
+
+public String readableFileSize(long size) {
+	    if(size <= 0) return "0";
+	    final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
+	    int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
+	    return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+}
