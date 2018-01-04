ff41f2efb6cb5a042ec60ee2a65e923efc2d883b
unknown <rschmidt@W0420494.ads.lwl.org>
2018-01-04 09:27:35 +0100

    'changes'

---
+++ java/gson.java
@@ -0,0 +1,17 @@
+//Pretty-Print JSON in Java
+
+
+Gson gson = new GsonBuilder().setPrettyPrinting().create();
+JsonParser jp = new JsonParser();
+JsonElement je = jp.parse(uglyJSONString);
+String prettyJsonString = gson.toJson(je);
+
+
+String json = new Gson().toJson(new Foo());
+
+
+<dependency>
+			<groupId>com.google.code.gson</groupId>
+			<artifactId>gson</artifactId>
+			<version>2.6.1</version>
+</dependency>  
